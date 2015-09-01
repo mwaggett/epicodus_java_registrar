76ff2411272127767e0286842bbbc2eaf4594176
Molly Waggett & Kallen Millner <student@epicodus.com>
2015-09-01 12:28:50 -0700

    Add web interface for viewing all students, adding new students, viewing all courses, and adding new courses.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Calendar;
 import java.lang.*;
 import static spark.Spark.*;
 import spark.ModelAndView;
@@ -22,16 +23,87 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    // post("/newpage", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //
-    //   <Class> param = request.queryParams("param");
-    //   model.put("param", param);
-    //
-    //   model.put("template", "templates/newpage.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    get("/students", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
 
+      List<Student> students = Student.all();
+      model.put("students", students);
+      model.put("template", "templates/students.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/students/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+      ArrayList<Integer> years = new ArrayList<Integer>();
+      for (int year = currentYear - 5; year <= currentYear + 5; year++) {
+        years.add(year);
+      }
+
+      HashMap<Integer, String> months = new HashMap<Integer, String>();
+      months.put(1, "January");
+      months.put(2, "February");
+      months.put(3, "March");
+      months.put(4, "April");
+      months.put(5, "May");
+      months.put(6, "June");
+      months.put(7, "July");
+      months.put(8, "August");
+      months.put(9, "September");
+      months.put(10, "October");
+      months.put(11, "November");
+      months.put(12, "December");
+
+      model.put("years", years);
+      model.put("months", months);
+      model.put("template", "templates/new-student-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/students/create", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      String name = request.queryParams("name");
+      int year = Integer.parseInt(request.queryParams("year"));
+      int month = Integer.parseInt(request.queryParams("month"));
+      int day = Integer.parseInt(request.queryParams("day"));
+
+      Student newStudent = new Student(name, year, month, day);
+      newStudent.save();
+
+      response.redirect("/students");
+      return null;
+    });
+
+    get("/courses", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      List<Course> courses = Course.all();
+      model.put("courses", courses);
+      model.put("template", "templates/courses.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/courses/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      model.put("template", "templates/new-course-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/courses/create", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      String name = request.queryParams("name");
+      String courseNumber = request.queryParams("coursenumber");
+
+      Course newCourse = new Course(name, courseNumber);
+      newCourse.save();
+
+      response.redirect("/courses");
+      return null;
+    });
   }
 
 }
